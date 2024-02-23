a851d4082075fabe8ffb8ec1714ba3eae5ea5779
Daniel Jankowski <djank0113@gmail.com>
2024-02-23 12:59:19 -0500

    finished sdlc assessment

--- sdlc.js
+++ sdlc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
 // Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +18,23 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const planning = 
+    "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+
+
+const analysisOfRequirements = 
+    "Further clarify features of the product and what requirements must be met to complete the project. The SDT might also create an analysis of resources to build the features in question, such as systems, labor, expenses, or other managerial aspects";
+
+
+
+const design = 
+    "create a product design that meets the requirements and constraints determined in the analysis of requirements  architectural 'blueprint' that ";
+
+const implementation = "Developers build the product according to the specifications of the design blueprint. Developers might be split up into teams to work on a specific feature"
+
+const testingAndIntegration =
+    "The code is tested by the QA team to find and fix any bugs or problems. Integration involves putting all of the individual units of code together and testing everything as a whole system to verify it meets the requirements and everything runs as expected.";
+
+const maintain =
+    "The SDT corrects any defects and works on enhancements or other changes required by the customer, company or user.";
