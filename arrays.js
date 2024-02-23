3d3ec0e49155006f5767f1230696e277595975f2
Daniel Jankowski <djank0113@gmail.com>
2024-02-23 13:19:43 -0500

    finished arrays.js

--- arrays.js
+++ arrays.js
@@ -1,14 +1,22 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
 
-const faveColors = ['red', 'green', 'black']
+const faveColors = ["red", "green", "black"];
 
 //CODE HERE
+
+colorCopy = faveColors.slice();
+console.log("PROBLEM 1: faveColors = " + faveColors);
+console.log("");
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+
+colorCopy.push("blue");
+console.log("PROBLEM 2: colorCopy = " + colorCopy);
+console.log("");
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -16,10 +24,14 @@ const faveColors = ['red', 'green', 'black']
   numbers array that will capture only the middle numbers (2, 3, 4).
 */
 
-const numbers = [1, 2, 3, 4, 5]
+const numbers = [1, 2, 3, 4, 5];
 
 //CODE HERE
 
+middleNums = numbers.slice(1, 4);
+
+console.log("PROBLEM 3: numbers = " + numbers);
+console.log("");
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -31,6 +43,18 @@ const numbers = [1, 2, 3, 4, 5]
   to the answers array. 
 */
 
-const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
+const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352];
 
 // CODE HERE
+
+let answers = [];
+
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+  if (bigOrSmallArray[i] > 100) {
+    answers.push(" big");
+  } else {
+    answers.push(" small");
+  }
+}
+
+console.log("PROBLEM 4: answers =" + answers);
